3c03bbb6ef06bea539bfb41876a82b2744f66330
John <exarhouleasjohn@gmail.com>
2021-06-15 15:50:55 -0400

    commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,14 @@ export default class Sidebar extends Component {
     render() {
         return (
             <div className='sidebar'>
-                
+                <ul className='sidebar-list'>
+                    <li className='sidebar-items'>History</li><hr/>
+                    <li className='sidebar-items'>Deposit</li><hr/>
+                    <li className='sidebar-items'>Withdraw</li><hr/>
+                    <li className='sidebar-items'>Loans</li><hr/>
+                    <li className='sidebar-items'>Details</li><hr/>
+                    <li className='sidebar-items'>Account</li><hr/>
+                </ul>
             </div>
         )
     }
